df7873d8032042e7920310b9c980be663db94847
DevilHe <929673054@qq.com>
2020-12-26 23:07:01 +0800

    feat:添加五子棋页面

---
+++ pages/gobang/gobang.vue
@@ -0,0 +1,51 @@
+<template>
+	<view class="gobang-content">
+		<view>
+			<canvas canvas-id="gobang" id="gobang" width="450px" height="450px" @error="canvasIdErrorCallback"></canvas>
+		</view>
+
+		<view class="handle">
+			<button class="handle-btn" @click="restart">重新开始</button>
+			<button class="handle-btn" @click="goback">悔棋</button>
+			<button class="handle-btn" @click="goreturn">撤销</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			canvasIdErrorCallback (e) {
+        console.error(e.detail.errMsg)
+			},
+			// 重新开始
+			restart () {
+        console.log('重新开始')
+			},
+			// 悔棋
+			goback () {
+        console.log('悔棋')
+      },
+			goreturn () {
+        console.log('撤销')
+      }
+		}
+	}
+</script>
+
+<style>
+.gobang-content .handle {
+	width: 100%;
+	padding: 10px 20px;
+}
+.gobang-content .handle .handle-btn {
+	float: left;
+	padding: 0 20px;
+	margin: 12px 2%;
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,8 +1,12 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
+		</view>
+
+		<view style="width: 100%;padding:10px 20px;">
+			<button style="float: left;width: 40%;margin: 12px 5%;" @click="btnGobang">五子棋</button>
+			<button style="float: left;width: 40%;margin: 12px 5%;" @click="btnSudoku">数独</button>
 		</view>
 	</view>
 </template>
@@ -11,42 +15,46 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: '请选择'
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			// 五子棋
+			btnGobang() {
+				uni.navigateTo({
+					url: '/pages/gobang/gobang'
+				})
+			},
+			// 数独
+			btnSudoku() {
+				// uni.navigateTo({
+				// 	url: '/pages//'
+				// })
+			}
 		}
 	}
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+.text-area {
+	display: flex;
+	justify-content: center;
+	margin: 12px auto;
+}
+.title {
+	font-size: 36rpx;
+	color: #8f8f94;
+}
+	
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	
 </style>
